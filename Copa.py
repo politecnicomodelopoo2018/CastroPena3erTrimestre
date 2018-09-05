443fa96281d7d4d948a5596c41c6c3dcd3a39bec
lucho-fran <francopenaaaaj2000@gmail.com>
2018-09-05 17:16:37 -0300

    cammiteando ando

--- Copa.py
+++ Copa.py
@@ -48,6 +48,8 @@ class Copa(object):
 
             return False
 
+
+
     @staticmethod
     def getCopa(unID):
 
--- main.py
+++ main.py
@@ -115,3 +115,170 @@ def Uefa():
 
 if __name__ == '__main__':  # para actualizar automaticamente la pagina sin tener que cerrarla
     app.run(debug=True) # para correr la pagina se puede hacer en este caso "python3 PruebaFlask.py" en la terminal
+
+
+opcion = input("pepe")
+
+while opcion != 8:
+
+    if opcion == 1:
+
+        opcionUno= input("1-ins"
+                        "2-del"
+                        "3-upt")
+
+        if opcionUno == 1:
+
+            #insertar
+
+        elif opcionUno == 2:
+
+            #deletear
+
+        elif opcionUno == 3:
+
+            #updatear
+
+        else:
+
+            opcion=input("se oprimio una opcion correcta, volviendo al menu")
+
+    elif opcion == 2:
+
+        opcionDos = input("1-ins"
+                          "2-del"
+                          "3-upt")
+
+        if opcionDos == 1:
+
+            #insertar
+
+        elif opcionDos == 2:
+
+            #deletear
+
+        elif opcionDos == 3:
+
+            #updatear
+
+        else:
+
+            opcion = input("se oprimio una opcion correcta, volviendo al menu")
+
+    elif opcion == 3:
+
+        opcionTres = input("1-ins"
+                          "2-del"
+                          "3-upt")
+
+        if opcionTres == 1:
+
+            # insertar
+
+        elif opcionTres == 2:
+
+            # deletear
+
+        elif opcionTres == 3:
+
+            # updatear
+
+        else:
+
+            opcion = input("se oprimio una opcion correcta, volviendo al menu")
+
+    elif opcion == 4:
+
+        opcionCuatro = input("1-ins"
+                          "2-del"
+                          "3-upt")
+
+        if opcionCuatro == 1:
+
+            # insertar
+
+        elif opcionCuatro == 2:
+
+            # deletear
+
+        elif opcionCuatro == 3:
+
+            # updatear
+
+        else:
+
+            opcion = input("se oprimio una opcion correcta, volviendo al menu")
+
+    elif opcion == 5:
+
+        opcionCinco = input("1-ins"
+                          "2-del"
+                          "3-upt")
+
+        if opcionCinco == 1:
+
+            # insertar
+
+        elif opcionCinco == 2:
+
+            # deletear
+
+        elif opcionCinco == 3:
+
+            # updatear
+
+        else:
+
+            opcion = input("se oprimio una opcion correcta, volviendo al menu")
+
+    elif opcion == 6:
+
+        opcionSix = input("1-ins"
+                          "2-del"
+                          "3-upt")
+
+        if opcionSix == 1:
+
+            # insertar
+
+        elif opcionSix == 2:
+
+            # deletear
+
+        elif opcionSix == 3:
+
+            # updatear
+
+        else:
+
+            opcion = input("se oprimio una opcion correcta, volviendo al menu")
+
+    elif opcion == 7:
+
+        opcionSiete = input("1-ins"
+                          "2-del"
+                          "3-upt")
+
+        if opcionSiete == 1:
+
+            # insertar
+
+        elif opcionSiete == 2:
+
+            # deletear
+
+        elif opcionSiete == 3:
+
+            # updatear
+
+        else:
+
+            opcion = input("se oprimio una opcion correcta, volviendo al menu")
+
+    elif opcion == 8:
+
+        exit()
+
+    else:
+
+        opcion = input("se oprimio una opcion correcta, volviendo al menu")
